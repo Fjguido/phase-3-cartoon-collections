f46fe38a3e6618b0ddb7706b7944c59abf9b42a9
Francis Guido <fjguido97@gmail.com>
2022-02-22 16:39:57 -0500

    commit tree f2a660735b929d810286ea576a9f4d180c451342

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+
+def roll_call_dwarves array 
   # Your code here
+  array.map.with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteers)
+  planeteers.map do |planeteer|
+    planeteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls array
   # Your code here
+  four_letters = array.find do |name|
+    name.length > 4
+  end
+  if four_letters
+    true
+  else false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |name|
+    cheese_types.include?(name)
+  end
 end
